718a29c5c069ecf6d6b4058f9ff8ce2a71406afd
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2023-09-10 17:47:29 -0300

    feat: created product model

---
+++ Model/Product.cs
@@ -0,0 +1,12 @@
+namespace api_crud;
+
+public class Product
+{
+  public int Id { get; set; }
+
+  public int Name { get; set; }
+
+  public int Description { get; set; }
+
+  public int Quantity { get; set; }
+}
